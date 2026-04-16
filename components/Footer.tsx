89f06f6907d601e41db5166c24192907b4a85741
likhith12gl <liki1962003@gmail.com>
2026-04-17 00:55:22 +0530

    fix: remove unused icons from footer to pass ESLint in prod build

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,17 +8,7 @@ const InstagramIcon = ({ size = 20 }: { size?: number | string }) => (
   </svg>
 );
 
-const YoutubeIcon = ({ size = 20 }: { size?: number | string }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17" /><path d="m10 15 5-3-5-3z" />
-  </svg>
-);
-
-const FacebookIcon = ({ size = 20 }: { size?: number | string }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
-  </svg>
-);
+// Removed unused icons to pass ESLint
 
 const quickLinks = [
   { href: "/", label: "Home" },
