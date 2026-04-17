aa5089bbbf3e95bba000d4144de8d9fffcc0c4f9
likhith12gl <liki1962003@gmail.com>
2026-04-18 00:18:03 +0530

    logo update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://kirans-fitness-club.vercel.app',
+  siteUrl: process.env.SITE_URL || 'https://kiransfitnessclub.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: 'weekly',
